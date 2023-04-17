48f4117e0338e525f15807f3705249d1ed1388fe
Rutuja Jadhav <rutujasamiksha123@gmail.com>
2023-04-17 11:25:15 +0530

    dice rolling game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Game from './Game';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [firstClick, setFirstClick] = React.useState(true)
+    const [isActive, setIsActive] = React.useState(false);
+    const [seconds, setSeconds] = React.useState(0);
+    
+    React.useEffect(() => {
+        
+        let timer = null;
+        if(isActive){
+            timer = setInterval(() => {
+                setSeconds((seconds) => seconds + 1);
+            }, 1000);
+        }
+        return () => {
+            clearInterval(timer);
+        };
+    });
+    
+    return (
+        <Game 
+            firstClick={firstClick}
+            setFirstClick={setFirstClick}
+            isActive={isActive}
+            setIsActive={setIsActive}
+            seconds={seconds}
+            setSeconds={setSeconds}
+        />
+    )
 }
 
 export default App;
